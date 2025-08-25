606f7dd78fafee220d1f1d7dd34d781b6d0ec427
Arushi Aloné <137710409+itzarushi16@users.noreply.github.com>
2025-08-26 00:01:16 +0530

    Update Student.java

--- src/main/java/com/lcwd/hiber/entities/Student.java
+++ src/main/java/com/lcwd/hiber/entities/Student.java
@@ -101,7 +101,7 @@ public class Student {
  // helper method to add certificate
     public void addCertificate(Certificate certificate) {
         certificates.add(certificate);     
-        certificate.setStudent(this);         // certificate ka student set kar do (back reference)
+        certificate.setStudent(this);        
     }
 
     // helper method to remove certificate
